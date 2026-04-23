3c3cedb86c6494b7fa73102fa10ab88328f58102
Rxzera-21 <rafamarinucci2007@gmail.com>
2026-04-22 22:19:04 -0300

    Rafael: Correção e identificação dos erros presentes no código enviado pelo professor

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include "stdio.h"
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; // Falta de ; depois da identificacao das variaveis.
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); // Falta do & no scanf da nota2.
 
-    media = nota1 + nota2 / 2;
+    media = nota1 + nota2 / 2; // Falta dos parenteses no calculo da meta, sem eles a ordem das operacoes feitas muda.
 
-    if (media = 6) {
+    if (media >= 6) { // Uso de = (atribuição) em vez de >= (comparação) no if
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
